a7388998758ae05414175c4e7ba82217116d466a
Jennifer.Zhan <jennifer.zhan.2015@gmail.com>
2024-06-01 00:47:52 -0400

    Updates

---
+++ component---src-pages-404-js-53993a9d901ba40b26c3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksinolux_site=self.webpackChunksinolux_site||[]).push([[125],{3331:function(e,t,s){s.r(t);var l=s(6540),n=s(1511),o=s(2269);t.default=()=>l.createElement(n.A,null,l.createElement(o.A,{title:"Not found"}),l.createElement("h1",{style:{position:"absolute",top:"50%",left:"50%"}},"NOT FOUND"),l.createElement("p",{style:{position:"absolute",top:"70%",left:"15%"}},"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-53993a9d901ba40b26c3.js.map
